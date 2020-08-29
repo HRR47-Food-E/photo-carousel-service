de71f8bd09d7c79736d9a0dfaec06058e8a8fb4c
Grace Lindelien <gracelindelien@gmail.com>
2020-08-29 14:38:41 -0700

    Clean up code

--- server/database/__data/__dataGeneration.js
+++ server/database/__data/__dataGeneration.js
@@ -9,7 +9,7 @@ const writeStream = fs.createWriteStream('./__data.csv');
 const target = 10000000;
 
 for (let i = 1; i <= target; i += 1) {
-  // Create object to store restaurant data
+  // Create string to store restaurant data
   let restaurantData = '';
   // Generate random fake restaurant name
   let name = faker.company.companyName();
@@ -19,16 +19,15 @@ for (let i = 1; i <= target; i += 1) {
   }
   name = name.join('');
   restaurantData += `${name},`;
-  // Create array to store photo filenames
+  // Create array to store photo filenumbers
   let photos = [];
   // Determine random number of photos for each restaurant (15-25)
   const photosPerRestaurant = Math.floor(Math.random() * 10 + 15 + 1);
-
-  // run while loop until photos length is equal to photosPerRestaurant
+  // run while loop until photo array's length is equal to photosPerRestaurant
   while (photos.length < photosPerRestaurant) {
     // generate file number between 0-332
     const fileNum = Math.floor(Math.random() * 333);
-    // create a new filename - example: img0001.jpeg OR img0010.jpeg OR img0100.jpeg
+    // create a new filename string - example: '0001' OR '0010' OR '0100'
     let filename;
     if (fileNum < 10) {
       filename = `000${fileNum}`;
@@ -37,17 +36,18 @@ for (let i = 1; i <= target; i += 1) {
     } else {
       filename = `0${fileNum}`;
     }
-    // check if photos array contains filename already, push if not
+    // if photos array does not include filename, push it
     if (!photos.includes(filename)) {
       photos.push(filename);
     }
   }
 
-  // attach photos to restaurant data object
+  // join photo array into string, each photo separated by a colon
   photos = photos.join(':');
+  // add photos to restaurant data string
   restaurantData += `${photos}\n`;
 
-  // write restaurant Data to file as a JSON string
+  // write restaurant data to file
   writeStream.write(restaurantData, 'utf8');
 }
 

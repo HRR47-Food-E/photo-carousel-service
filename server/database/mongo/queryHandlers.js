4063ffbcd23c944b971e50fe01e1e7bffc86b6c4
Grace Lindelien <gracelindelien@gmail.com>
2020-08-31 12:25:33 -0700

    Implement query to find record #10000000 in MongoDB

--- server/database/mongo/queryHandlers.js
+++ server/database/mongo/queryHandlers.js
@@ -12,3 +12,21 @@ db
     console.log(`There was a problem connecting to mongo at: ${mongoURI}`);
     console.log(err);
   });
+
+const restaurantSchema = new mongoose.Schema({
+  id: Number,
+  name: String,
+  images: String,
+});
+
+const Restaurants = mongoose.model('Restaurants', restaurantSchema);
+
+// console.time('find');
+Restaurants.find({ id: 10000000 }, (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+    // console.timeEnd('find');
+  }
+});

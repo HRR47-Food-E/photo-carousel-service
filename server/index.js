ee24c46778adb3bb5c28c7cefc9683dc73f87167
Grace Lindelien <gracelindelien@gmail.com>
2020-09-02 15:34:48 -0700

    Add execution time testing to all CRUD operations

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 // const path = require('path');
 const cors = require('cors');
 const db = require('./database/postgres/queryHandlers.js');
-// const db = require('./database/mongo/queryHandlers.js');
 
 const app = express();
 const port = 3001;
@@ -32,34 +31,40 @@ app.get('/restaurant/:id', (req, res) => {
 
 // Create new restaurant
 app.post('/restaurant', (req, res) => {
+  console.time('POST');
   db.addRestaurant(req.body, (err, data) => {
     if (err) {
       res.status(500).send(`An internal error occurred: ${err}`);
     } else {
       res.status(201).send(data);
     }
+    console.timeEnd('POST');
   });
 });
 
 // Update restaurant by ID
 app.put('/restaurant/:id', (req, res) => {
+  console.time('PUT');
   db.updateRestaurant(req, (err) => {
     if (err) {
       res.status(500).send(`An error occurred: ${err}`);
     } else {
       res.status(200).send(`Restaurant ${req.params.id} has been updated with the name: ${req.body.name}`);
     }
+    console.timeEnd('PUT');
   });
 });
 
 // Delete restaurant by ID
 app.delete('/restaurant/:id', (req, res) => {
+  console.time('DELETE');
   db.deleteRestaurant(req, (err) => {
     if (err) {
       res.status(500).send(`An error occurred: ${err}`);
     } else {
       res.status(200).send(`Restaurant ${req.params.id} has been deleted`);
     }
+    console.timeEnd('DELETE');
   });
 });
 

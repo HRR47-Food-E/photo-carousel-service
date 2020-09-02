3917e9c58aeb36a412bb4eef54e0bbae257d8049
Grace Lindelien <gracelindelien@gmail.com>
2020-09-02 14:03:18 -0700

    Delete code for old API routes

--- server/index.js
+++ server/index.js
@@ -38,43 +38,6 @@ app.post('/restaurant', (req, res) => {
   });
 });
 
-// // Create new restaurant
-// app.post('/api/add-restaurant', (req, res) => {
-//   const { name } = req.body;
-//   db.addRestaurant(name, (err) => {
-//     if (err) {
-//       res.status(500).send(`An error occurred: ${err}`);
-//     } else {
-//       res.status(201).send(`${name} was added to the Restaurant database!`);
-//     }
-//   });
-// });
-
-// // Update restaurant
-// app.put('/api/update-restaurant/:id', (req, res) => {
-//   const { id } = req.params;
-//   const { name } = req.body;
-//   db.updateRestaurant(id, name, (err) => {
-//     if (err) {
-//       res.status(500).send(`An error occurred: ${err}`);
-//     } else {
-//       res.status(200).send(`Restaurant ${id} has been updated with the name: ${name}`);
-//     }
-//   });
-// });
-
-// // Delete restaurant
-// app.delete('/api/delete-restaurant/:id', (req, res) => {
-//   const { id } = req.params;
-//   db.deleteRestaurant(id, (err) => {
-//     if (err) {
-//       res.status(500).send(`An error occurred: ${err}`);
-//     } else {
-//       res.status(200).send(`Restaurant ${id} has been deleted`);
-//     }
-//   });
-// });
-
 app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`App listening on ${port}`);
